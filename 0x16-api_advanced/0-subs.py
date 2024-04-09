d0055b84542b83dc5b3902e4965b682e9dc310c6
Bakijazzman <obafemiolabayo@gmail.com>
2024-04-09 20:39:32 +0100

    subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Import modules Here """
+import requests
+
+def number_of_subscribers(subreddit):
+    """ gets the number of subscribers from a given subreddit
+    
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "Bakijazzman"}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
