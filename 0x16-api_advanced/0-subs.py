7e4982a67efda783a440e653b05806983d7435a4
Bakijazzman <obafemiolabayo@gmail.com>
2024-04-09 21:03:44 +0100

    codestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
     """ gets the number of subscribers from a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "Bakijazzman"}
+    header = {
+            "User-Agent": "Bakijazzman"
+            }
     response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 404:
         return 0
